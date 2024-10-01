aaea4b48339871e620327f1cabf763361febc3a2
Frangeris Peguero <frangeris@users.noreply.github.com>
2024-10-01 20:50:54 -0300

    Add tests specs

---
+++ __tests__/options.spec.ts
@@ -0,0 +1,21 @@
+import Options from "@/options";
+
+describe("options", () => {
+  it("should return a singleton instance", () => {
+    const instance1 = Options.instance();
+    const instance2 = Options.instance();
+    expect(instance1).toBe(instance2);
+  });
+
+  it("should have undefined opts initially", () => {
+    const instance = Options.instance();
+    expect(instance.opts).toBeUndefined();
+  });
+
+  it("should set and retrieve opts correctly", () => {
+    const instance = Options.instance();
+    const opts = { base: "http://example.com" };
+    instance.opts = opts;
+    expect(instance.opts).toEqual(opts);
+  });
+});
