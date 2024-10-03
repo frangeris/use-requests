f171e0b72433d145451b6b58df910589109fe362
Frangeris Peguero <frangeris@users.noreply.github.com>
2024-10-02 23:50:41 -0300

    Simplify `init` implementation

--- src/init.ts
+++ src/init.ts
@@ -1,29 +1,24 @@
 import Service from "./service";
 import Options from "./options";
-
-let gbl = globalThis as unknown as { services: any };
+import context from "./context";
 
 export function init(baseURL: string, endpoints: Record<string, string>) {
   Options.instance().opts = { baseURL };
-
-  const services: Record<string, Service<unknown>> = {};
-  for (const endpoint in endpoints) {
-    const key = endpoint as keyof typeof endpoints;
-    services[key] = new Service<"">(endpoints[endpoint]);
+  const requests = {};
+  for (const key in endpoints) {
+    Object.assign(requests, {
+      [key]: new Service<unknown>(endpoints[key]),
+    });
   }
 
-  if (typeof gbl.services === "undefined") {
-    Object.defineProperty(globalThis, "services", {
-      value: services,
+  if (typeof context.useRequests === "undefined") {
+    Object.defineProperty(globalThis, "useRequests", {
+      value: requests,
       enumerable: false,
       configurable: true,
       writable: true,
     });
   }
-}
-
-export function getServices() {
-  return gbl.services;
 }
 
 export default init;
