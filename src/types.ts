2e0dab721fab7e37f042ea2196f425e43398998e
Frangeris Peguero <frangeris@users.noreply.github.com>
2024-10-11 01:11:21 -0300

    refactor: Change implementation of data property (#23)
    * Remove condition to parse, use try/catch instead
    
    * Changed type of data
    
    * Updated tests

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ export type InitOptions = {
 
 export type Services<T> = { [K in keyof T]: Service<T[K]> };
 
-export type ServiceResponse<R> = { data: R | null } & Response;
+export type ServiceResponse<R> = { data: () => Promise<R | null> } & Response;
 
 export type RequestPath<P> =
   | {
