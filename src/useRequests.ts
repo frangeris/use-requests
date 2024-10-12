df06a106a28b8e7dd12812eaf1a03b9cbe5f0a59
Frangeris Peguero <frangeris@users.noreply.github.com>
2024-10-12 18:56:00 -0300

    Throw an error when useRequests is called without initializing context first

--- src/useRequests.ts
+++ src/useRequests.ts
@@ -2,6 +2,11 @@ import { Services } from "./types";
 import context from "./context";
 
 export function useRequest<T>() {
+  const ctx = context.useRequests;
+  if (!ctx) {
+    throw new Error("init must be called before using useRequest");
+  }
+
   return context.useRequests as Services<T>;
 }
 
