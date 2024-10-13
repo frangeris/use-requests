85b4788a3d0281b98be0ac766ef845480ee398a1
Frangeris Peguero <frangeris@users.noreply.github.com>
2024-10-13 15:36:41 -0300

    Updated tests

--- __tests__/useRequests.spec.ts
+++ __tests__/useRequests.spec.ts
@@ -1,8 +1,11 @@
+import type { ServiceResponse } from "@/types";
 import { useRequests, init } from "@/index";
 import context from "@/context";
 
 global.fetch = jest.fn(() => ({
-  json: () => Promise.resolve({}),
+  status: 200,
+  json: () => Promise.resolve(),
+  data: {},
 })) as jest.Mock;
 
 enum Api {
@@ -26,6 +29,12 @@ describe("useRequests", () => {
     const result = useRequests<typeof Api>();
     expect(result).toBe(context.useRequests);
     expect(result).toHaveProperty("test");
+  });
+
+  it("should return an instance of ServiceResponse", async () => {
+    const { test } = useRequests<typeof Api>();
+    const r = await test.get();
+    expect(r).toHaveProperty("data");
   });
 
   it("should make a GET request", async () => {
@@ -57,6 +66,12 @@ describe("useRequests", () => {
     const { status } = await test.patch([
       { op: "add", path: "/test", value: "test" },
     ]);
+    expect(status).toBe(200);
+  });
+
+  it("should make a request with query params", async () => {
+    const { test } = useRequests<typeof Api>();
+    const { status } = await test.get({ query: { test: "test" } });
     expect(status).toBe(200);
   });
 });
