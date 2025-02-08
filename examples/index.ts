65e9c09475b851c9b4de8f2f63c0c5dc7eaf0a56
Frangeris Peguero <frangeris@users.noreply.github.com>
2025-02-07 23:05:24 -0300

    Update examples

--- examples/index.ts
+++ examples/index.ts
@@ -1,16 +1,21 @@
 import { init, useRequests, useRawRequest, useOptions } from "../src";
 
-// Test the library with the JSONPlaceholder API
-// https://jsonplaceholder.typicode.com/
+const base = "https://jsonplaceholder.typicode.com/";
 
 // can be defined globally
 enum Api {
   posts = "/posts",
   comments = "/comments",
 }
-init("https://jsonplaceholder.typicode.com", {
+
+init({
+  base,
   endpoints: Api,
-  options: { cache: "no-store" },
+  request: { cache: "no-store" },
+  throwOnError: false,
+  interceptors: {
+    onerror: (r) => {},
+  },
 });
 
 const typed = async () => {
@@ -18,28 +23,28 @@ const typed = async () => {
   const { posts, comments } = useRequests<typeof Api>();
 
   let t = await posts.get();
-  console.log(t.status, "GET /posts");
+  console.log(t?.status, "GET /posts");
 
   t = await posts.post({});
-  console.log(t.status, "POST /posts");
+  console.log(t?.status, "POST /posts");
 
   t = await posts.put({}, { path: "/:id", params: { id: 1 } });
-  console.log(t.status, "PUT /posts/1");
+  console.log(t?.status, "PUT /posts/1");
 
   t = await posts.patch([{ op: "add", path: "/tests", value: true }], {
     path: "/:id",
     params: { id: 1 },
   });
-  console.log(t.status, "PATCH /posts/1");
+  console.log(t?.status, "PATCH /posts/1");
 
   t = await posts.delete({ path: "/:id", params: { id: 1 } });
-  console.log(t.status, "DELETE /posts/1");
+  console.log(t?.status, "DELETE /posts/1");
 
   t = await posts.get({ path: "/:id/comments", params: { id: 1 } });
-  console.log(t.status, "GET /posts/1/comments");
+  console.log(t?.status, "GET /posts/1/comments");
 
   t = await comments.get({ query: { postId: 1 } });
-  console.log(t.status, "GET /comments?postId=1");
+  console.log(t?.status, "GET /comments?postId=1");
   console.log("\n");
 };
 
@@ -47,10 +52,10 @@ const typed = async () => {
 const raw = async () => {
   console.info("⚡️ Raw requests");
   const raw = useRawRequest();
-  const r = await raw("https://jsonplaceholder.typicode.com");
+  const r = await raw(base);
 
   let t = await r.get("/posts");
-  console.log(t.status, "GET /posts");
+  console.log(t?.status, "GET /posts");
 
   // parameters in url are also supported and can be passed as an object too
   // but it doesn't throw an error if the parameter is not passed, so params
@@ -59,17 +64,20 @@ const raw = async () => {
     path: "/posts/:id",
     params: { id: 1 },
   });
-  console.log(t.status, "GET /posts/1");
+  console.log(t?.status, "GET /posts/1");
 
   t = await r.get({
     path: `/posts/:id\\:top`,
     params: { id: 1 },
+    query: {
+      test: [1, 2, 3].join(","),
+    },
   });
-  console.log(t.status, "GET /posts/1:top");
+  console.log(t?.status, `GET ${t?.url}`);
 };
 
 // run all examples sequentially
 (async () => {
-  await typed();
+  // await typed();
   await raw();
 })();
